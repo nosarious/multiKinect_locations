c09fdf8d28473312daf91b159e9b2ae563676eea
Gerry Straathof <nosemonger@gmail.com>
2012-01-03 14:23:53 -0700

    Simplification
    I've removed all instances of the collision detection from this code to
    make it easier to understand.
    
    This code simply reproduces the point cloud across the entire array of
    kinects. In the future it will incorporate a moving portion that allows
    you to find out where the different kinects are.

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -4,64 +4,9 @@
 //--------------------------------------------------------------
 void testApp::setup() {
 
-	/* setup for the particle system */
-    //load our image inside bin/data
-    image.loadImage ( "Stairwell2.jpg" ) ; 
-    //if the app performs slowly raise this number
-    sampling = 60 ; 
-	bDrawSquares = true;
-    
-    //Retrieve the pixels from the loaded image
-    unsigned char * pixels = image.getPixels() ;
-    //store width and height for optimization and clarity
- /*   int w = image.width ; 
-    int h = image.height ; 
-	*/
-	int w = 5760;
-	int h = 1080;
-
-	delayedStart = 0; //add a jitter every ten updates or so
-	wRatio = ofGetWidth()/w;
-	hRatio = ofGetHeight()/h;
-    
-    //offsets to center the particles on screen
-/*    int xOffset = (ofGetWidth() - w ) /2 ; 
-    int yOffset = (ofGetHeight() - h ) /2 ;
-    */
-	int xOffset = 0;
-	int yOffset = 0;
-    //Loop through all the rows
-    for ( int x = 0 ; x < w ; x+=sampling ) 
-    {
-        //Loop through all the columns
-        for ( int y = 0 ; y < h ; y+=sampling ) 
-        {
-            //Pixels are stored as unsigned char ( 0 <-> 255 ) as RGB
-            //If our image had transparency it would be 4 for RGBA
-            int index = ( y * w + x ) * 3 ; 
-            ofColor color ; 
-            color.r = pixels[index] ;       //red pixel
-            color.g = pixels[index+1] ;     //blue pixel
-            color.b = pixels[index+2] ;     //green pixel
-            particles.push_back( Particle ( ofPoint ( x + xOffset , y + yOffset ) , color, ofPoint(0,0,0) ) ) ;  
-        }
-    }
-    
-    ofSetFrameRate( 10 ) ; 
-    numParticles = ( image.width * image.height ) / sampling ; 
-    
-    //Set spring and sink values
-    cursorMode = 0 ; 
-    forceRadius = sampling*4 ; 
-    friction = 0.015 ; 
-    springFactor = 0.12 ; 
-    springEnabled = true ; 
-
-
-	/* setup for kinect stuff */
+	/// setup for kinect stuff 
     ofSetLogLevel(OF_LOG_VERBOSE);
 
-	
 	kinect.close();
     kinect2.close();
     kinect3.close();
@@ -70,8 +15,6 @@ void testApp::setup() {
     kinect6.close();
 
 	kinect.init(false, false);
-	//kinect.init(true);  // shows infrared instead of RGB video image
-	//kinect.init(false, false);  // disable video image (faster fps)
 	kinect.setVerbose(true);
 	kinect.open();
 
@@ -90,30 +33,13 @@ void testApp::setup() {
     kinect6.init(false, false);
     kinect6.open();
 	
-	// start with the live kinect source
-	kinectSource = &kinect;
 
-	colorImg.allocate(kinect.width, kinect.height);
-	grayImage.allocate(kinect.width, kinect.height);
-	grayThreshNear.allocate(kinect.width, kinect.height);
-	grayThreshFar.allocate(kinect.width, kinect.height);
-
-	nearThreshold = 230;
-	farThreshold  = 70;
-	bThreshWithOpenCV = true;
 
 	ofSetFrameRate(30);
 
-	bRecord = false;
-	bPlayback = false;
+	stepNumber = 4;
+	
 
-	// zero the tilt on startup
-	angle = 0;
-//	kinect.setCameraTiltAngle(angle);
-
-	// start from the front
-	pointCloudRotationY = 180;
-	bDrawPointCloud = true;
 
 	cameraWidth = 640;
 	cameraSpace = ofGetWidth()/6;
@@ -153,43 +79,8 @@ void testApp::setup() {
 //--------------------------------------------------------------
 void testApp::update() {
 	ofBackground(0,0,0);
-	
-	activatePoint(mouseX , mouseY);
 
-	delayedStart = delayedStart + 1;
-	if (delayedStart >10){
-		delayedStart = 0;
-		activatePoint(ofRandom(ofGetWidth()),ofRandom(ofGetHeight()));
-	}
-
-	ofPoint diff ;          //Difference between particle and mouse
-    float dist ;            //distance from particle to mouse ( as the crow flies ) 
-    float ratio ;           //Ratio of how strong the effect is = 1 + (-dist/maxDistance) ;
-   
-    //Create an iterator to cycle through the vector
-    std::vector<Particle>::iterator p ; 
-    for ( p = particles.begin() ; p != particles.end() ; p++ ) 
-    {
-        ratio = 1.0f ; 
-        p->velocity *= friction ; 
-        //reset acceleration every frame
-        p->acceleration = ofPoint() ; 
-        
-        if ( springEnabled ) 
-        {
-            //Move back to the original position
-            p->acceleration.x += springFactor * (p->spawnPoint.x - p->position.x);
-            p->acceleration.y += springFactor * (p->spawnPoint.y - p->position.y) ;
-        }
-        
-        p->velocity += p->acceleration * ratio ; 
-        p->position += p->velocity ; 
-		p->angle.z += p->velocity.y;
-		p->angle.x += p->velocity.x;
-		p->angle.y += p->velocity.y;
-    }
-
-/* update for kinect system */
+// update for kinect system 
 	cameraWidth = 640;
 	cameraSpace = ofGetWidth()/6;
 	screenRatio = cameraSpace/cameraWidth;
@@ -213,68 +104,12 @@ void testApp::update() {
 
 //--------------------------------------------------------------
 void testApp::draw() {
-	
-	image.draw(ofGetWidth()/2,ofGetHeight()/2,ofGetWidth(),ofGetHeight());
-/* drawing the particle system */
-if (bDrawSquares){
-	ofSetRectMode(OF_RECTMODE_CENTER);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0,ofGetWidth(),ofGetHeight(),0,-200,200);
-	glMatrixMode(GL_MODELVIEW);
-	gluLookAt(0,200,1200,0,0,0,0,1,0);
-		// enable blending
-	glEnable(GL_BLEND);
-	ofTexture tex = image.getTextureReference();
-	tex.bind();
-	std::vector<Particle>::iterator p;
-	for (p = particles.begin(); p!=particles.end(); p++)
-	{
-	//	glColor3ub((unsigned char)p->color.r,(unsigned char)p->color.g,(unsigned char)p->color.b);
-		ofSetColor(255,255,255,25);
-		ofPushMatrix();
-		glLoadIdentity();
-		ofPushStyle();
-		
-		if (bDrawSquares){
-		//	ofTranslate(p->position.x,p->position.y);
-		//	ofRotateX(p->angles.x);
-		//	ofRotateY(p->angles.y);
-			glTranslatef(p->position.x,p->position.y,0);
-			ofRotateZ(p->angle.z);
-			ofRotateY(p->angle.y);
-			ofRotateX(p->angle.x);
-		//	ofSetColor(50,50,50,100);
-		//	ofRect(0,0,0,sampling+10,sampling+10);
-			glScalef(sampling*1.5,sampling*1.5,1);
-			glBegin(GL_QUADS);
-				glTexCoord2f(p->position.x,p->position.y);
-				glVertex3f(0,0,0);
-				glTexCoord2f(p->position.x+sampling,p->position.y);
-				glVertex3f(1,0,0);
-				glTexCoord2f(p->position.x+sampling,p->position.y+sampling);
-				glVertex3f(1,1,0);
-				glTexCoord2f(p->position.x,p->position.y+sampling);
-				glVertex3f(0,1,0);
 
-			glEnd();
-		//	ofRect(1,1,sampling,sampling);
-		}
-		ofPopStyle();
-	ofPopMatrix();
-	}
 
-		// enable blending
-	glEnable(GL_BLEND);
-}
+	// drawing the kinect system
 
-/* drawing the kinect system */
 	ofSetRectMode(OF_RECTMODE_CENTER);
 	ofSetColor(255,255,255);
-	
-		
-
-
 		ofPushMatrix();
 			ofTranslate(kleft+kleftAdd, kup);
 			drawPointCloud3(kleft+kleftAdd, kup);
@@ -323,235 +158,131 @@ if (bDrawSquares){
 
 }
 
-void testApp::activatePoint(int xValue, int yValue){
-	
-	ofPoint diff ;          //Difference between particle and mouse
-    float dist ;            //distance from particle to mouse ( as the crow flies ) 
-    float ratio ;           //Ratio of how strong the effect is = 1 + (-dist/maxDistance) ;
-    const ofPoint mousePosition = ofPoint( xValue , yValue ) ; //Allocate and retrieve mouse values once.
-
-    
-    //Create an iterator to cycle through the vector
-    std::vector<Particle>::iterator p ; 
-    for ( p = particles.begin() ; p != particles.end() ; p++ ) 
-    {
-        ratio = 1.0f ; 
-        p->velocity *= friction ; 
-        //reset acceleration every frame
-        p->acceleration = ofPoint() ; 
-        diff = mousePosition - p->position ;  
-        dist = ofDist( 0 , 0 , diff.x , diff.y/3 ) ;
-        //If within the zone of interaction
-        if ( dist < forceRadius )  
-        {
-            ratio = -1 + dist / forceRadius ; 
-            //Repulsion
-            if ( cursorMode == 0 ) 
-                p->acceleration -= ( diff * ratio) ;
-            //Attraction
-            else
-                p->acceleration += ( diff * ratio ) ; 
-        }
-		/*
-        if ( springEnabled ) 
-        {
-            //Move back to the original position
-            p->acceleration.x += springFactor * (p->spawnPoint.x - p->position.x);
-            p->acceleration.y += springFactor * (p->spawnPoint.y - p->position.y) ;
-        }
-        */
-        p->velocity += p->acceleration * ratio ; 
-        p->position += p->velocity ; 
-		p->angle.z += p->velocity.y;
-		p->angle.x += p->velocity.x;
-		p->angle.y += p->velocity.y;
-		
-    }
-}
-
 void testApp::drawPointCloud(int left,int  up) {
 	int w = 640;
 	int h = 480;
-	int step = 60;
+	int step = stepNumber;
 	for(int y = 0; y < h; y += step) {
 		for(int x = 0; x < w; x += step) {
 			ofVec3f cur = kinect.getWorldCoordinateFor(x, y);
 			ofColor color = kinect.getCalibratedColorAt(x,y);
-		//	glColor3ub((unsigned char)color.r,(unsigned char)color.g,(unsigned char)color.b);
-			ofSetColor(ofColor::fromHsb(sinf(cur.x)*128+128,255,255));
-		//	glVertex3f(cur.x, cur.y, cur.z);
-		//	ofRect(480-x,cur.y,10,10);
-		//	ofRect(screenRatio-x*kinectRatio,cur.z*(ofGetHeight()/2),10,10);
-		//	ofRect((cameraWidth-x)*screenRatio-sinf(cur.x),cur.z*(ofGetHeight()/2),10,10);
 			int z = cur.z*(ofGetHeight()/2);
 			float xRatio=-screenRatio*2*((z+100)*tanTimes)/(camera2Screen/2);
 			int newX = xRatio*((320-x));
 			if ((cur.z*(ofGetHeight()/2)<1080)&&(cur.z*(ofGetHeight()/2)>50)){
-				if (cur.y>0.20){
-					ofCircle(newX,z,20);
+				if (cur.y>0.10){
+					ofCircle(newX,z,2);
 				}
-				ofRect(newX,z,10,10);
-				
-					activatePoint(newX+left,z+up);
+				ofRect(newX,z,1,1);
 			}
 		}
 	}
-	glEnd();
 }
 
 void testApp::drawPointCloud2(int left,int  up) {
 	int w = 640;
 	int h = 480;
-	//glBegin(GL_POINTS);
-	int step = 60;
+	int step = stepNumber;
 	for(int y = 0; y < h; y += step) {
 		for(int x = 0; x < w; x += step) {
 			ofVec3f cur = kinect2.getWorldCoordinateFor(x, y);
 			ofColor color = kinect2.getCalibratedColorAt(x,y);
-		//	glColor3ub((unsigned char)color.r,(unsigned char)color.g,(unsigned char)color.b);
-			ofSetColor(ofColor::fromHsb(sinf(cur.x)*128+128,255,255));
-		//	glVertex3f(cur.x, cur.y, cur.z);
-		//	ofRect(480-x,cur.y,10,10);
-		//	ofRect(screenRatio-x*kinectRatio,cur.z*(ofGetHeight()/2),10,10);
-		//	ofRect((cameraWidth-x)*screenRatio-sinf(cur.x),cur.z*(ofGetHeight()/2),10,10);
 			int z = cur.z*(ofGetHeight()/2);
 			float xRatio=-screenRatio*2*((z+100)*tanTimes)/(camera2Screen/2);
 			int newX = xRatio*((320-x));
 			if ((cur.z*(ofGetHeight()/2)<1080)&&(cur.z*(ofGetHeight()/2)>50)){
-				if (cur.y>0.20){
-					ofCircle(newX,z,20);
+				if (cur.y>0.10){
+					ofCircle(newX,z,2);
 				}
-				ofRect(newX,z,10,10);
-					activatePoint(newX+left,z+up);
+				ofRect(newX,z,1,1);
 			}
 		}
 	}
-	glEnd();
 }
 
 void testApp::drawPointCloud3(int left,int  up) {
 	int w = 640;
 	int h = 480;
-	//glBegin(GL_POINTS);
-	int step = 60;
+	int step = stepNumber;
 	for(int y = 0; y < h; y += step) {
 		for(int x = 0; x < w; x += step) {
 			ofVec3f cur = kinect3.getWorldCoordinateFor(x, y);
 			ofColor color = kinect3.getCalibratedColorAt(x,y);
-		//	glColor3ub((unsigned char)color.r,(unsigned char)color.g,(unsigned char)color.b);
-			ofSetColor(ofColor::fromHsb(sinf(cur.x)*128+128,255,255));
-		//	glVertex3f(cur.x, cur.y, cur.z);
-		//	ofRect(480-x,cur.y,10,10);
-		//	ofRect(screenRatio-x*kinectRatio,cur.z*(ofGetHeight()/2),10,10);
-		//	ofRect((cameraWidth-x)*screenRatio-sinf(cur.x),cur.z*(ofGetHeight()/2),10,10);
 			int z = cur.z*(ofGetHeight()/2);
 			float xRatio=-screenRatio*2*((z+100)*tanTimes)/(camera2Screen/2);
 			int newX = xRatio*((320-x));
 			if ((cur.z*(ofGetHeight()/2)<1080)&&(cur.z*(ofGetHeight()/2)>50)){
-				if (cur.y>0.20){
-					ofCircle(newX,z,20);
+				if (cur.y>0.10){
+					ofCircle(newX,z,2);
 				}
-				ofRect(newX,z,10,10);
-					activatePoint(newX+left,z+up);
+				ofRect(newX,z,1,1);
 			}
 		}
 	}
-	glEnd();
 }
 
 void testApp::drawPointCloud4(int left,int  up) {
 	int w = 640;
 	int h = 480;
-	//glBegin(GL_POINTS);
-	int step = 60;
+	int step = stepNumber;
 	for(int y = 0; y < h; y += step) {
 		for(int x = 0; x < w; x += step) {
 			ofVec3f cur = kinect4.getWorldCoordinateFor(x, y);
 			ofColor color = kinect4.getCalibratedColorAt(x,y);
-			glColor3ub((unsigned char)color.r,(unsigned char)color.g,(unsigned char)color.b);
-			ofSetColor(ofColor::fromHsb(sinf(cur.x)*128+128,255,255));
-			
-		//	ofRect(480-x,cur.y,10,10);
-		//	ofRect(screenRatio-x*kinectRatio,cur.z*(ofGetHeight()/2),10,10);
-		//	ofRect((cameraWidth-x)*screenRatio-sinf(cur.x),cur.z*(ofGetHeight()/2),10,10);
 			int z = cur.z*(ofGetHeight()/2);
+			ofSetColor(ofColor::fromHsb(sinf(cur.z)*128+128,255,255));
 			float xRatio=-screenRatio*2*((z+100)*tanTimes)/(camera2Screen/2);
 			int newX = xRatio*((320-x));
 			if ((cur.z*(ofGetHeight()/2)<1080)&&(cur.z*(ofGetHeight()/2)>50)){
-				if (cur.y>0.20){
-					ofCircle(newX,z,20);
+				if (cur.y>0.10){
+					ofCircle(newX,z,2);
 				}
-				ofRect(newX,z,10,10);
-					activatePoint(newX+left,z+up);
+				ofRect(newX,z,1,1);
 			}
 		}
 	}
-	glEnd();
 }
 
 void testApp::drawPointCloud5(int left,int  up) {
 	int w = 640;
 	int h = 480;
-	int step = 60;
+	int step = stepNumber;
 	for(int y = 0; y < h; y += step) {
 		for(int x = 0; x < w; x += step) {
 			ofVec3f cur = kinect5.getWorldCoordinateFor(x, y);
 			ofColor color = kinect5.getCalibratedColorAt(x,y);
-		//	glColor3ub((unsigned char)color.r,(unsigned char)color.g,(unsigned char)color.b);
-			ofSetColor(ofColor::fromHsb(sinf(cur.x)*128+128,255,255));
-		//	glVertex3f(cur.x, cur.y, cur.z);
-		//	ofRect(480-x,cur.y,10,10);
-		//	ofRect(screenRatio-x*kinectRatio,cur.z*(ofGetHeight()/2),10,10);
-		//	ofRect((cameraWidth-x)*screenRatio-sinf(cur.x),cur.z*(ofGetHeight()/2),10,10);
 			int z = cur.z*(ofGetHeight()/2);
 			float xRatio=-screenRatio*2*((z+100)*tanTimes)/(camera2Screen/2);
 			int newX = xRatio*((320-x));
 			if ((cur.z*(ofGetHeight()/2)<1080)&&(cur.z*(ofGetHeight()/2)>50)){
-				if (cur.y>0.20){
-					ofCircle(newX,z,20);
+				if (cur.y>0.10){
+					ofCircle(newX,z,2);
 				}
-				ofRect(newX,z,10,10);
-					activatePoint(newX+left,z+up);
+				ofRect(newX,z,1,1);
 			}
 		}
 	}
-	glEnd();
 }
 
 void testApp::drawPointCloud6(int left,int  up) {
 	int w = 640;
 	int h = 480;
-	int step = 60;
+	int step = stepNumber;
 	for(int y = 0; y < h; y += step) {
 		for(int x = 0; x < w; x += step) {
 			ofVec3f cur = kinect6.getWorldCoordinateFor(x, y);
 			ofColor color = kinect6.getCalibratedColorAt(x,y);
-		//	glColor3ub((unsigned char)color.r,(unsigned char)color.g,(unsigned char)color.b);
-			ofSetColor(ofColor::fromHsb(sinf(cur.x)*128+128,255,255));
-		//	glVertex3f(cur.x, cur.y, cur.z);
-		//	ofRect(480-x,cur.y,10,10);
-		//	ofRect(screenRatio-x*kinectRatio,cur.z*(ofGetHeight()/2),10,10);
-		//	ofRect((cameraWidth-x)*screenRatio-sinf(cur.x),cur.z*(ofGetHeight()/2),10,10);
 			int z = cur.z*(ofGetHeight()/2);
 			float xRatio=-screenRatio*2*((z+100)*tanTimes)/(camera2Screen/2);
 			int newX = xRatio*((320-x));
 			if ((cur.z*(ofGetHeight()/2)<1080)&&(cur.z*(ofGetHeight()/2)>50)){
-				if (cur.y>0.20){
-					ofCircle(newX,z,20);
+				if (cur.y>0.10){
+					ofCircle(newX,z,2);
 				}
-	
-				ofRect(newX,z,10,10);
-					activatePoint(newX+left,z+up);
-				/*
-				ofSetColor(255, 255, 255);
-					stringstream reportStream;
-					reportStream << "xratio " << xRatio << endl;
-					ofDrawBitmapString(reportStream.str(),newX,z);
-					*/
+				ofRect(newX,z,1,1);
 			}
 		}
 	}
-	glEnd();
 }
 
 //--------------------------------------------------------------
@@ -567,18 +298,7 @@ void testApp::exit() {
 //--------------------------------------------------------------
 void testApp::keyPressed (int key) {
 	switch (key) {
-		case ' ':
-			bThreshWithOpenCV = !bThreshWithOpenCV;
-		break;
-
-		case'p':
-			bDrawPointCloud = !bDrawPointCloud;
-			break;
-
-		case'b':
-			bDrawSquares = !bDrawSquares;
-			break;
-
+		
 		case '>':
 			multiplier1= multiplier1 +.05;
 		//	if (farThreshold > 255) farThreshold = 255;
@@ -591,22 +311,20 @@ void testApp::keyPressed (int key) {
 
 		case '+':
 			screenRatio= screenRatio +0.05;
-			if (nearThreshold > 255) nearThreshold = 255;
+			
 			break;
 
 		case '-':
 			screenRatio= screenRatio -0.05;
-			if (nearThreshold < 0) nearThreshold = 0;
+			
 			break;
 
 
 		case 'o':
-		//	kinect.setCameraTiltAngle(angle);	// go back to prev tilt
 			kinect.open();
 			break;
 
 		case 'c':
-		//	kinect.setCameraTiltAngle(0);		// zero the tilt
 			kinect.close();
 			break;
 
@@ -691,43 +409,12 @@ void testApp::keyPressed (int key) {
 		case 'Y':
 			kup6= kup6 -1;
 			break;
-/*
-		case 'r':
-			bRecord = !bRecord;
-			if(bRecord) {
-				startRecording();
-			} else {
-				stopRecording();
-			}
-			break;
-
-		case 'q':
-			bPlayback = !bPlayback;
-			if(bPlayback) {
-				startPlayback();
-			} else {
-				stopPlayback();
-			}
-			break;
-
-		case OF_KEY_UP:
-			angle++;
-			if(angle>30) angle=30;
-			kinect.setCameraTiltAngle(angle);
-			break;
-
-		case OF_KEY_DOWN:
-			angle--;
-			if(angle<-30) angle=-30;
-			kinect.setCameraTiltAngle(angle);
-			break;
-			*/
 	}
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y) {
-	pointCloudRotationY = x;
+	
 }
 
 //--------------------------------------------------------------
@@ -746,39 +433,4 @@ void testApp::mouseReleased(int x, int y, int button)
 void testApp::windowResized(int w, int h)
 {}
 
-//--------------------------------------------------------------
-void testApp::startRecording() {
 
-	// stop playback if running
-	stopPlayback();
-
-	kinectRecorder.init(ofToDataPath("recording.dat"));
-	bRecord = true;
-}
-
-//--------------------------------------------------------------
-void testApp::stopRecording() {
-	kinectRecorder.close();
-	bRecord = false;
-}
-
-//--------------------------------------------------------------
-void testApp::startPlayback() {
-
-	stopRecording();
-	kinect.close();
-
-	// set record file and source
-	kinectPlayer.setup(ofToDataPath("recording.dat"), true);
-	kinectPlayer.loop();
-	kinectSource = &kinectPlayer;
-	bPlayback = true;
-}
-
-//--------------------------------------------------------------
-void testApp::stopPlayback() {
-	kinectPlayer.close();
-	kinect.open();
-	kinectSource = &kinect;
-	bPlayback = false;
-}
--- src/testApp.h
+++ src/testApp.h
@@ -3,10 +3,6 @@
 #include "ofMain.h"
 #include "ofxOpenCv.h"
 #include "ofxKinect.h"
-#include "Particle.h"
-
-// uncomment this to read from two kinects simultaneously
-#define USE_TWO_KINECTS
 
 class testApp : public ofBaseApp {
 	public:
@@ -31,44 +27,15 @@ class testApp : public ofBaseApp {
 		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
 
-		void activatePoint(int xValue, int yValue);
-
-		/// start/stop recording and playback,
-		/// make sure you don't record and playback simultaneously
-		/// using the same file!!!
-		void startRecording();
-		void stopRecording();
-		void startPlayback();
-		void stopPlayback();
 
 
 		ofxKinect 			kinect;
 		ofxKinectRecorder 	kinectRecorder;
 		ofxKinectPlayer 	kinectPlayer;
 
-		/// used to switch between the live kinect and the recording player
-		ofxBase3DVideo* 	kinectSource;
-
-		ofxCvColorImage		colorImg;
-
-		ofxCvGrayscaleImage grayImage;			// grayscale depth image
-		ofxCvGrayscaleImage grayThreshNear;		// the near thresholded image
-		ofxCvGrayscaleImage grayThreshFar;		// the far thresholded image
-
-		ofxCvContourFinder 	contourFinder;
-
-		bool				bThreshWithOpenCV;
-		bool				bDrawPointCloud;
-
-		int 				nearThreshold;
-		int					farThreshold;
 
 		int					angle;
 
-		int 				pointCloudRotationY;
-
-		bool 				bRecord;
-		bool 				bPlayback;
 
         // second kinect for testing
         ofxKinect 			kinect2;
@@ -86,6 +53,7 @@ class testApp : public ofBaseApp {
 		
         // third kinect for testing
         ofxKinect 			kinect6;
+
 		int kleft,kleft2,kleft3,kleft4,kleft5,kleft6;
 		int kleftAdd,kleft2Add,kleft3Add,kleft4Add,kleft5Add,kleft6Add;
 		int kup,kup1,kup2,kup3,kup4,kup5,kup6;
@@ -99,21 +67,9 @@ class testApp : public ofBaseApp {
 
 /* variables for particle system */
 		       
-        ofImage image ;                 //image to load
-        int sampling ;                  //pixels to skip
-        vector<Particle> particles ;    //vector to store pixels
-        int numParticles ;          //Number of particles 
-       
-        //Spring and Sink Factors
-        bool springEnabled ;        //toggle whether particles return to their origin
-        float forceRadius ;             //radius of repellent/attraction force
-        float friction ;                //damping to slow the particles down
-        float springFactor ;          //how much the particle "springs" back to origin
-        int cursorMode ;            
+            
     
-		bool bDrawSquares;
-
-		int delayedStart;
 	int wRatio;
 	int hRatio;
+	int stepNumber;
 };
